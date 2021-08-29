6d8e2a15c71c300b3e00f2d365448d0937b5af78
Filipe Silva <sousadasilvafilipe@gmail.com>
2021-08-29 16:00:38 -0300

    Atualizando, (hh:mm:ss:ms)

--- script.js
+++ script.js
@@ -6,19 +6,22 @@ function carregar() {
 
   function intervalo() {
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 21
     const minutos = data.getMinutes();
     const segundos = data.getSeconds();
     const milisegundos = data.getMilliseconds()
+
+    if (hora >= 00 && hora <= 12) {
+      img.src = 'imagem/manha.jpg'
+    } else if (hora >= 13 && hora < 18) {
+      img.src = 'imagem/tarde.png'
+    } else {
+      img.src = 'imagem/noite.png'
+    }
     msg.innerHTML = `Agora são ${hora}:${minutos}:${segundos}:${milisegundos}`
   }
   setInterval(intervalo, 1)
 
-  if (hora >= 00 && hora <= 12) {
-    img.src = 'imagem/manha.jpg'
-  } else if (hora >= 13 && hora < 18) {
-    img.src = 'imagem/tarde.png'
-  } else {
-    img.src = 'imagem/noite.png'
-  }
+  
 }
