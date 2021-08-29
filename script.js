4f4b23e936e654e4abd6e996a78b38c0c0f6ddc6
Filipe Silva <sousadasilvafilipe@gmail.com>
2021-08-29 15:45:24 -0300

    Atualizando a hora em tempo real.

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ function carregar() {
   var img = window.document.getElementById('imagem')
 
   var msg = window.document.getElementById('msg')
-  var data = new Date()
-  //var hora = data.getHours()
-  var hora =8
-  msg.innerHTML = `Agora são ${hora} horas.`
+
+
+  function intervalo() {
+    var data = new Date()
+    var hora = data.getHours()
+    const minutos = data.getMinutes();
+    const segundos = data.getSeconds();
+    const milisegundos = data.getMilliseconds()
+    msg.innerHTML = `Agora são ${hora}:${minutos}:${segundos}:${milisegundos}`
+  }
+  setInterval(intervalo, 1)
 
   if (hora >= 00 && hora <= 12) {
-    img.src ='imagem/manha2-removebg-preview.png'
-} else if (hora >= 13 && hora < 18) {
-    img.src ='imagem/tarde2-removebg-preview.png'
+    img.src = 'imagem/manha.jpg'
+  } else if (hora >= 13 && hora < 18) {
+    img.src = 'imagem/tarde.png'
   } else {
-    img.src ='imagem/noite2.png'
+    img.src = 'imagem/noite.png'
   }
 }
